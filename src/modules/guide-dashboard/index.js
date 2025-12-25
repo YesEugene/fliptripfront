33093e9614de7a1ce054bc386e02ea2a58cc78f9
Eugene <yes@Mac.Home>
2025-12-25 13:53:35 +0000

    fix: Comment out EditTourPage export from guide-dashboard module

--- src/modules/guide-dashboard/index.js
+++ src/modules/guide-dashboard/index.js
@@ -4,7 +4,8 @@
 
 export { default as GuideDashboardPage } from './pages/GuideDashboardPage';
 export { default as CreateTourPage } from './pages/CreateTourPage';
-export { default as EditTourPage } from './pages/EditTourPage';
+// EditTourPage removed - editing now done in Trip Visualizer
+// export { default as EditTourPage } from './pages/EditTourPage';
 export { default as ProfileSettingsPage } from './pages/ProfileSettingsPage';
 export { default as TripVisualizerPage } from './pages/TripVisualizerPage';
 
